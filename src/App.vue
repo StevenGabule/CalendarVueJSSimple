ba1b7850f882d347ace22c5eb5b034e99ffb31e8
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-27 11:33:33 +0800

    simple calendar app using vuejs

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <CalendarWeek />
+    <CalendarEntry />
+  </div>
+</template>
+
+<script>
+  import CalendarWeek from "./components/CalendarWeek";
+  import CalendarEntry from "./components/CalendarEntry";
+  export default {
+    name: 'App',
+    components: {CalendarEntry, CalendarWeek}
+  }
+</script>
+
+<style lang="scss">
+  html,body {
+    height: 100%;
+  }
+  #app {}
+  #calender-week {}
+  #calender-entry {}
+</style>
+
+<style lang="scss" scoped>
+  #app {
+    height: inherit;
+    background-color: #e6e6e6;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
